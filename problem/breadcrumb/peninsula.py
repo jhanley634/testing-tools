b1466c553643e361ea33528264e920b8301f2cd1
John Hanley <jhanley634@gmail.com>
2017-08-27 11:15:31 -0700

    Delete unused linear tranform.

--- problem/breadcrumb/peninsula.py
+++ problem/breadcrumb/peninsula.py
@@ -22,36 +22,6 @@ import matplotlib.pyplot as plt
 import os
 
 
-class Xform1:
-    '''Models 1-D linear transformation between lng/lat coord and pixels.'''
-
-    def __init__(self, min_coord, max_coord, max_px):
-        self.min_coord = min_coord
-        self.coord_range = max_coord - min_coord
-        self.max_px = max_px
-        assert self.coord_range > 0
-
-    def to_px(self, coord_val):
-        delta = coord_val - self.min_coord
-        assert 0 <= delta <= self.coord_range, delta  # Must stay in bounds.
-        return self.max_px * (delta / self.coord_range)
-
-
-class Xform2:
-    '''Models 2-D linear transformation between lng/lat coord and pixels.'''
-
-    def __init__(self, coord_nw, coord_se, width_height_px):
-        # NorthWest is upper left and SouthEast is lower right.
-        # Those happened to be the locations that were readily measurable.
-        # Origin is SouthEast, lower left. Pass in (lng, lat) coordinates.
-        self.lng_xform = Xform1(coord_nw[0], coord_se[0], width_height_px[0])
-        self.lat_xform = Xform1(coord_se[1], coord_nw[1], width_height_px[1])
-
-    def to_px(self, coord_val):
-        lng, lat = coord_val
-        return self.lng_xform(lng), self.lat_xform(lat)
-
-
 def markup_peninsula_map():
 
     img = plt.imread('topoquest-peninsula.jpg')
@@ -59,10 +29,7 @@ def markup_peninsula_map():
     pruneyard_17 = -121.918, 37.285
     extent = (100 + willow_101[0], 100 + pruneyard_17[0],
               pruneyard_17[1], willow_101[1])
-    xform = Xform2(willow_101, pruneyard_17,
-                   img.shape[:2])  # Shape also mentions 3, # of RGB channels.
     fig, ax = plt.subplots()
-    # ax.set_aspect('equal')
     ax.imshow(img, extent=extent)
     print(fig, ax)
     plt.show()
