9485641d3f2e855e06f7fd4658a32f6f713d1108
John Hanley <john.hanley@parc.com>
2018-09-23 11:07:49 -0700

    Adds seaborn pairplot.

--- problem/regress1/regress.py
+++ problem/regress1/regress.py
@@ -18,26 +18,54 @@
 # arising from, out of or in connection with the software or the use or
 # other dealings in the software.
 
-# https://machinelearningmastery.com/feature-selection-machine-learning-python/
 
-# Feature Extraction with Univariate Statistical Tests (Chi-squared for classification)
-import pandas
-import numpy
+from pathlib import Path
+
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
-# load data
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-dataframe = pandas.read_csv(url, names=names)
-array = dataframe.values
-X = array[:,0:8]
-Y = array[:,8]
-# feature extraction
-test = SelectKBest(score_func=chi2, k=4)
-fit = test.fit(X, Y)
-# summarize scores
-numpy.set_printoptions(precision=3)
-print(fit.scores_)
-features = fit.transform(X)
-# summarize selected features
-print(features[0:5,:])
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy
+import pandas
+import seaborn as sns
+
+
+def report(fspec='~/Desktop/data.csv', pair=False):
+    df = (pandas.read_csv(Path(fspec).expanduser())
+          .drop('foo', axis=1))
+    print(df[0:3])
+    ncols = df.shape[1] - 1
+    array = df.values
+    X = array[:, 0:ncols]
+    Y = array[:, ncols]
+    find_good_features(X, Y)
+    regress(df.median_dom.values.reshape(-1, 1), df.q3_dom.values)
+    if pair:
+        sns.pairplot(df)
+
+
+def regress(X, y):
+    lr = LinearRegression()
+    lr.fit(X, y)
+    fig, ax = plt.subplots()
+    ax.scatter(y, lr.predict(X), edgecolors=(0, 0, 0))
+    ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+    ax.set_xlabel('Measured')
+    ax.set_ylabel('Predicted')
+    plt.show()
+
+
+# https://machinelearningmastery.com/feature-selection-machine-learning-python/
+# Feature Extraction with Univariate Statistical Tests
+# (Chi-squared for classification)
+def find_good_features(X, Y, nfeat=2):
+    test = SelectKBest(score_func=chi2, k=nfeat)
+    fit = test.fit(X, Y)
+    numpy.set_printoptions(precision=3)
+    print(fit.scores_)
+    features = fit.transform(X)
+    print(features[0:nfeat, :])
+
+
+if __name__ == '__main__':
+    report()
