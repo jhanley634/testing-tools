37b65e59683effc0802b38766439a6850a9c5cf8
John Hanley <john.hanley@parc.com>
2019-01-26 14:05:58 -0800

    Scales the Y direction.

--- problem/stores_and_cust/sd_space.py
+++ problem/stores_and_cust/sd_space.py
@@ -5,9 +5,13 @@ import geopy.distance
 class SdSpace:
     """San Diego space is a metric space that maps lat, longs of
     the continental 48, originally located in quadrant II, to
-    a portion of quadrant I, with San Diego somewhat near the origin."""
+    a portion of quadrant I, with San Diego somewhat near the origin.
+    Degrees are converted to a number that approximately corresponds to meters.
+    """
 
     X_SCALE = 4_149_754 / 44.791  # meters per degree of longitude (SD->Miami)
+
+    Y_SCALE = 1_710_540 / 23.610  # meters per degree of latitude (SD->Seattle)
 
     ORIGIN_LAT = 24
     """24 degrees N latitude,
@@ -32,13 +36,13 @@ class SdSpace:
                 or lng > self.MAINE):
             raise ValueError(f'coord ({lat}, {lng}) out of bounds')
         self.x = int((lng - self.ORIGIN_LNG) * self.X_SCALE)
-        self.y = lat - self.ORIGIN_LAT
+        self.y = int((lat - self.ORIGIN_LAT) * self.Y_SCALE)
 
     def lng(self):
         return self.x / self.X_SCALE + self.ORIGIN_LNG
 
     def lat(self):
-        return self.y + self.ORIGIN_LAT
+        return self.y / self.Y_SCALE + self.ORIGIN_LAT
 
     def distance(self, other):
         """Great circle distance in meters between two geographic points."""
--- problem/stores_and_cust/sd_space_test.py
+++ problem/stores_and_cust/sd_space_test.py
@@ -11,33 +11,33 @@ class SdSpaceTest(unittest.TestCase):
 
     def test_sd_space(self):
         san_diego = self.san_diego
-        self.assertEqual(32.715, san_diego.lat())
+        self.assertAlmostEqual(32.715, san_diego.lat(), places=4)
         self.assertAlmostEqual(-117.163, san_diego.lng(), places=4)
 
         self.assertEqual(726_074, san_diego.x)
-        self.assertAlmostEqual(8.715, san_diego.y)
+        self.assertEqual(631_400, san_diego.y)
 
     def test_distance(self):
         miami = SdSpace(25.775, -80.209)
         self.assertEqual(4_149_754, miami.x)
-        self.assertAlmostEqual(1.775, miami.y)
+        self.assertEqual(128_598, miami.y)
 
         self.assertEqual(3_654_467, int(self.san_diego.distance(miami)))
 
         phoenix = SdSpace(33.450, -112.067)
-        self.assertEqual(1198204, phoenix.x)
-        self.assertAlmostEqual(9.450, phoenix.y)
+        self.assertEqual(1_198_204, phoenix.x)
+        self.assertEqual(684_650, phoenix.y)
 
         self.assertEqual(482_673, int(self.san_diego.distance(phoenix)))
 
         seattle = SdSpace(47.610, -122.333)
         self.assertEqual(247_089, seattle.x)
-        self.assertAlmostEqual(23.610, seattle.y)
+        self.assertEqual(1_710_539, seattle.y)
 
-        self.assertEqual(1_710_540, int(self.san_diego.distance(seattle)))
+        self.assertEqual(1_710_539, int(self.san_diego.distance(seattle)))
 
         augusta = SdSpace(44.310, -69.780)
         self.assertEqual(5_115_970, augusta.x)
-        self.assertAlmostEqual(20.310, augusta.y)
+        self.assertEqual(1_471_455, augusta.y)
 
         self.assertEqual(4_263_237, int(self.san_diego.distance(augusta)))
