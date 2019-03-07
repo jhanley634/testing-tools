6ef4a45f44792dafa534758b210515636a70b8c6
John Hanley <john.hanley@parc.com>
2019-03-06 21:13:49 -0800

    Adds as_dict helper.

--- problem/timeline/app.py
+++ problem/timeline/app.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from flask_api import FlaskAPI
+import numpy as np
 
 from problem.timeline.presidents import get_us_presidents
 
@@ -9,13 +10,24 @@ app = FlaskAPI(__name__)
 df = get_us_presidents()
 
 
-@app.route('/president')
-def president():
-    row = df.loc[43]
-    for i, row in df.iterrows():
-        print('\n'.join(dir(row)))
-    print(row)
-    return {'result': row.to_dict()}
+def _make_serializable(x):
+    """Returns a JSON-serializable version of the input x."""
+    if isinstance(x, np.int64):
+        return int(x)
+    return x
+
+
+def as_dict(row):
+    return {k: _make_serializable(v)
+            for k, v in row.to_dict().items()}
+
+
+# e.g. http://localhost:5000/president/42
+
+@app.route('/president/<int:id>')
+def president(id):
+    row = df.iloc[id - 1]
+    return {'result': as_dict(row)}
 
 
 if __name__ == '__main__':
