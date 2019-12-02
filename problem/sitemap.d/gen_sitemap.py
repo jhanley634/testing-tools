c40b8d1dfe0cbfdf21b4107c2abb7e51f409eec7
John Hanley <john.hanley@parc.com>
2019-12-01 18:31:57 -0800

    Add iso func.

--- problem/sitemap.d/gen_sitemap.py
+++ problem/sitemap.d/gen_sitemap.py
@@ -25,6 +25,11 @@ import os
 from lxml import etree
 
 
+def _iso(sec: float) -> str:
+    stamp = dt.datetime.fromtimestamp(sec, dt.timezone.utc)
+    return stamp.isoformat()
+
+
 def _get_urls(top_dir='.', ext='.html'):
     p = 1
     pfx = os.path.abspath(top_dir) + os.path.sep
@@ -32,43 +37,45 @@ def _get_urls(top_dir='.', ext='.html'):
         for file in files:
             if file.endswith(ext):
                 path = os.path.join(root, file)
-                stamp = int(os.path.getmtime(path))
+                sec = int(os.path.getmtime(path))
                 path = os.path.abspath(path)[len(pfx):]
                 p *= .9999  # Newest entries should be crawled first.
                 yield dict(loc=path,
-                           loastmod=dt.datetime.fromtimestamp(stamp),
+                           lastmod=_iso(sec),
                            priority=round(p, 7))
 
 
+def _item_func(parent):
+    return 'url'
+    # yield dicttoxml(d, root=False, custom_root='url',
+    #   attr_type=False, item_func=_item_func).decode()
+
+
 def _get_xml_body():
-    for url in _get_urls(ext='.txt'):
-        yield str(url)
+    for d in _get_urls(ext='.txt'):
+        yield '  <url>'
+        for k, v in d.items():
+            yield f'    <{k}>{v}</{k}>'
+        yield '  </url>'
+    yield '</urlset>'
 
 
 def _get_preamble():
     return '''<?xml version="1.0"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
 '''
-
-
-def _get_postamble():
-    return '</urlset>\n'
 
 
 def gen_sitemap():
 
-    # dtd = etree.DTD()
-    parser = etree.XMLParser(dtd_validation=True, no_network=False, ns_clean=True)
-    f = io.StringIO(_get_preamble() + '\n'.join(_get_xml_body()) + _get_postamble())
-    tree = etree.parse(f, parser)
-
-    import pprint
-    pprint.pprint(dir(parser.resolvers))
-
-    for url in _get_urls(ext='.txt'):
-        print(url)
+    # parser = etree.XMLParser(dtd_validation=True, no_network=False, ns_clean=True)
+    f = io.StringIO(_get_preamble() + '\n'.join(_get_xml_body()))
+    etree.parse(f)  # , parser)
+    print(f.getvalue())
 
 
 if __name__ == '__main__':
