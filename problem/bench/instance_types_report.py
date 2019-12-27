d3a57720f2e313b66d1a7eb88efc91281aad6103
John Hanley <john.hanley@parc.com>
2019-12-27 10:32:56 -0800

    Report on relevant AWS EC2 instance types.

--- problem/bench/instance_types_report.py
+++ problem/bench/instance_types_report.py
@@ -23,8 +23,61 @@ from pathlib import Path
 import pandas as pd
 
 
+def _to_dollars(s: str):
+    s = s.replace(' USD per Hour', '')
+    return float(s)
+
+
+def _cols_to_delete():
+    return [
+        'Instance family',
+        'Instance size',
+        'Availability zones',
+        'Free-Tier eligible',
+        'Bare metal',
+        'Hypervisor',
+        'Dedicated Host support',
+        'On-Demand Hibernation support',
+        'Burstable Performance support',
+        'Valid cores',
+        'Local instance storage',
+        'Storage type',
+        'Storage disk count',
+        'EBS encryption support',
+        'EBS optimization support',
+        'Network performance',
+        'ENA support',
+        'Maximum number of network interfaces',
+        'IPv4 addresses per interface',
+        'IPv6 addresses per interface',
+        'IPv6 support',
+        'Supported placement group strategies',
+        'Auto Recovery support',
+        'Supported root devices',
+        'Current generation',
+    ]
+
+
 def report():
     in_fspec = Path(f'{__file__}/../instancetypes.csv').resolve()
+    df = pd.read_csv(in_fspec)
+    del df['On-Demand Windows pricing']
+    df['On-Demand Linux pricing'] = (
+        df['On-Demand Linux pricing'].apply(_to_dollars))
+    df = df.rename(columns={'On-Demand Linux pricing': 'price',
+                            'Threads per core': 'tpc',
+                            'Valid threads per core': 'vtpc',
+                            'Sustained clock speed (GHz)': 'GHz'})
+    df = df[df['Current generation']]
+    df = df[df.vCPUs > 8]
+    for col_name in _cols_to_delete():
+        if col_name:
+            del df[col_name]
+    df['price/core'] = df['price'] / df['vCPUs']
+    df.sort_values(by=['price/core', 'vCPUs'], inplace=True)
+    df.to_csv('/tmp/t.csv', index=False)
+
+    print(df.head(40))
 
 
 if __name__ == '__main__':
