67721950dcaaf7121c2dd4743f1196cf991407e0
John Hanley <jhanley@rexhomes.com>
2020-01-02 16:14:08 -0800

    Add charge observation model.

---
+++ problem/charge_state/model.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+# Copyright 2020 John Hanley.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# The software is provided "AS IS", without warranty of any kind, express or
+# implied, including but not limited to the warranties of merchantability,
+# fitness for a particular purpose and noninfringement. In no event shall
+# the authors or copyright holders be liable for any claim, damages or
+# other liability, whether in an action of contract, tort or otherwise,
+# arising from, out of or in connection with the software or the use or
+# other dealings in the software.
+
+from sqlalchemy.orm import scoped_session, sessionmaker
+import sqlalchemy as sa
+import sqlalchemy.ext.declarative
+
+
+Base = sqlalchemy.ext.declarative.declarative_base()
+session = scoped_session(sessionmaker())
+
+
+class ChargeObservation(Base):
+    __tablename__ = 'charge_observation'
+    id = sa.Column(sa.Integer(), primary_key=True)
+    stamp = sa.Column(sa.DateTime())
