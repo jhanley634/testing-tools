338037b1b05afc6692133983e9ffcc4581a541b1
John Hanley <jhanley@rexhomes.com>
2020-02-17 19:48:46 -0800

    Adds get_smaller_acl.
    We can still emit sub-optimal ACL rules, e.g.:
        40.77.167.32/27
        40.77.167.64/27
    which should be combined to a /26.

--- problem/weblog/prefix/compress_to_prefixes.py
+++ problem/weblog/prefix/compress_to_prefixes.py
@@ -19,8 +19,6 @@
 # other dealings in the software.
 
 
-from collections import deque
-
 import click
 
 from problem.weblog.prefix.ip_addr import IpAddr, Prefix, log_dist
@@ -29,36 +27,55 @@ from problem.weblog.prefix.ip_addr import IpAddr, Prefix, log_dist
 MAX_ACL_ENTRIES = 60  # Some amazon security groups, e.g. impose this quota limit.
 
 
+def _get_distances(acl):
+    prev = acl[-1]  # We anticipate high distance between first and last
+    for pfx in acl:
+        yield log_dist(prev.ip, pfx.ip)
+        prev = pfx
+
+
 def compress(ips, max_acl_entries=MAX_ACL_ENTRIES):
-    # This ordering brings together IPs that belong in a common prefix.
-    acl = deque(sorted(Prefix(ip, 32)
-                       for ip in ips))
+    # Sorting by IP brings together addresses that belong in a common prefix.
+    acl = [Prefix(ip, 32)
+           for ip in sorted(ips)]
 
-    while len(acl) > max_acl_entries:
-        _greedy_merge(acl)
+    # Need to merge together this many neighboring IPs.
+    n = max(0, len(acl) - max_acl_entries)
 
-    print("")
+    while n > 0:
+        distances = list(_get_distances(acl))
+        print(distances)
+        acl = _get_smaller_acl(acl, n, min(distances))
+        n = max(0, len(acl) - max_acl_entries)
+
     print("\n".join(map(str, acl)))
 
 
-def _greedy_merge(acl):
-    assert len(acl) >= 2
-    a = acl.popleft()
-    b = acl[0]
-    print("\n", a, b, end="\t")
-    b.masklen = min(a.masklen, b.masklen)
-    while a.ip not in b:
-        b.masklen -= 1
-    b = Prefix(b.ip, b.masklen)  # This clears the hostpart.
-    print(log_dist(a.ip, b.ip), f"{b.mask():x}")
+def _get_smaller_acl(acl, n, min_dist):
+    ret = []
+    prev = None
+    for pfx in acl:
+        if (n > 0
+                and prev
+                and log_dist(prev.ip, pfx.ip) <= min_dist):
+            n -= 1
+            pfx = _merge(ret.pop(), pfx.ip)
+        ret.append(pfx)
+        prev = pfx
+    return ret
 
+
+def _merge(pfx, ip):
+    while ip not in pfx:
+        pfx.masklen -= 1
+    return Prefix(pfx.ip, pfx.masklen)  # This clears the hostpart.
 
 
 @click.command()
 @click.option("--infile", required=True)
 def main(infile):
     with open(infile) as fin:
-        compress(map(IpAddr, fin.readlines()), 25)
+        compress(map(IpAddr, fin.readlines()), 23)
 
 
 if __name__ == "__main__":
