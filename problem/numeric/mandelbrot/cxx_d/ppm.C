bfffca2cbd39f54d212f223bb35e02007ec265d0
John Hanley <john.hanley@parc.com>
2020-02-23 17:49:57 -0800

    Add ppm.C

---
+++ problem/numeric/mandelbrot/cxx_d/ppm.C
@@ -0,0 +1,39 @@
+
+// Copyright 2020 John Hanley.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// The software is provided "AS IS", without warranty of any kind, express or
+// implied, including but not limited to the warranties of merchantability,
+// fitness for a particular purpose and noninfringement. In no event shall
+// the authors or copyright holders be liable for any claim, damages or
+// other liability, whether in an action of contract, tort or otherwise,
+// arising from, out of or in connection with the software or the use or
+// other dealings in the software.
+
+#include <iostream>
+using namespace std;
+
+class PPM {
+    public:
+
+    int size_px;
+
+    PPM(unsigned int px) {
+        size_px = px;
+        cout << "P3" << endl;
+        cout << size_px << endl;
+        cout << 255 << endl;
+    }
+
+    void plot(unsigned int grey_value) {
+        int v = grey_value;
+        cout << v << " " << v << " " << v << endl;
+    }
+};
