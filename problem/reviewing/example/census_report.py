cf7f783a166e573a535f15313a4f0e77c67f60ad
John Hanley <john.hanley@parc.com>
2020-07-22 15:54:43 -0700

    Add get_prospect_zips.

--- problem/reviewing/example/census_report.py
+++ problem/reviewing/example/census_report.py
@@ -4,22 +4,38 @@ import csv
 
 import uszipcode
 
+search = uszipcode.SearchEngine()
 
-def get_good_zips(out_fspec="/tmp/census.csv"):
-    """Finds ZIPS for which Census reports home values."""
-    search = uszipcode.SearchEngine()
-    # zipcode_type is {PO Box, Standard, Unique},
-    # and each type may list home values.
-    select = """
-        SELECT zipcode
-        FROM   simple_zipcode
-        WHERE  median_home_value IS NOT NULL
-    """
+
+def _rows_to_file(select, out_fspec):
     with open(out_fspec, "w") as fout:
         sheet = csv.writer(fout)
         for row in search.ses.execute(select):
             sheet.writerow(row)
 
 
+def get_good_zips(out_fspec="/tmp/census.csv"):
+    """Finds ZIPS for which Census reports home values."""
+    # zipcode_type is {PO Box, Standard, Unique},
+    # and each type may list home values.
+    select = """
+        SELECT  zipcode
+        FROM    simple_zipcode
+        WHERE   median_home_value IS NOT NULL
+    """
+    _rows_to_file(select, out_fspec)
+
+
+def get_prospect_zips(out_fspec="/tmp/prospects.csv"):
+    select = """
+        SELECT  zipcode
+        FROM    simple_zipcode
+        WHERE   major_city = 'Los Angeles'
+                AND state = 'CA'
+    """
+    _rows_to_file(select, out_fspec)
+
+
 if __name__ == '__main__':
     get_good_zips()
+    get_prospect_zips()
