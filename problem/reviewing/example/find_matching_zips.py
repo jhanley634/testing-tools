f3a1f17099cecc921984e81b9fd4b404a9d01926
John Hanley <john.hanley@parc.com>
2020-07-22 16:00:19 -0700

    Add (deliberately inefficient) count_matches()

--- problem/reviewing/example/find_matching_zips.py
+++ problem/reviewing/example/find_matching_zips.py
@@ -8,6 +8,17 @@ def get_rows(fspec_csv):
         yield from sheet
 
 
-def count_matches(census_csv, los_angeles_prospects_csv):
-    census = sorted(get_rows(census_csv))
-    for
+def count_matches(
+        census_csv="/tmp/census.csv",
+        los_angeles_prospects_csv="/tmp/prospects.csv"):
+    count = 0
+    for zipcode in get_rows(los_angeles_prospects_csv):
+        census = sorted(get_rows(census_csv))
+        if zipcode in census:
+            count += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    assert 60 == count_matches()
