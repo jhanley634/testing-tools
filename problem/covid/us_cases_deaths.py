5004851770fa964e52386d5048d0fa2e40a2aeeb
John Hanley <john.hanley@parc.com>
2020-10-11 18:21:46 -0700

    Add get_nyt_covid19_top_dir

--- problem/covid/us_cases_deaths.py
+++ problem/covid/us_cases_deaths.py
@@ -1,3 +1,36 @@
-#! /usr/bin/env python
+#! /usr/bin/env streamlit run
 
-import streamlit
+from pathlib import Path
+import datetime as dt
+
+# import altair as alt
+import pandas as pd
+import streamlit as st
+
+
+def _get_nyt_covid19_top_dir():
+    repo_top = Path(__file__ + '/../../..').resolve()
+    sibling_repo = repo_top / '../covid-19-data'  # NY Times repo
+    return sibling_repo.resolve()
+
+
+def get_cases_and_deaths():
+    #            date    cases  deaths
+    # 0    2020-01-21        1       0
+    # ..          ...      ...     ...
+    # 263  2020-10-10  7748030  214184
+
+    df = pd.read_csv(_get_nyt_covid19_top_dir() / 'us.csv')
+    df.date = pd.to_datetime(df.date)
+    return df
+
+
+def main():
+    df = get_cases_and_deaths()
+    print(dt.datetime.now().replace(microsecond=0))
+    st.line_chart(df)
+    # st.altair_chart(df)
+
+
+if __name__ == '__main__':
+    main()
