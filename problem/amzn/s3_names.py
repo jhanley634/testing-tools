f0b68f2b6b9148c4e693e51b69bf4daa1a35516e
John Hanley <john.hanley@parc.com>
2020-10-16 06:51:30 -0700

    Add store_names

--- problem/amzn/s3_names.py
+++ problem/amzn/s3_names.py
@@ -19,6 +19,8 @@
 # other dealings in the software.
 
 from pathlib import Path
+import subprocess
+
 import boto3
 import click
 
@@ -26,14 +28,33 @@ import click
 class S3Names:
 
     def __init__(self, bucket, directory):
-        self.bucket = bucket
+        self.bucket_name = bucket
         self.directory = Path(directory)
         assert self.directory.exists(), directory
 
     @property
     def _cache_file(self):
         PREFIX = 'bucket_'  # Arbitrary. This simply sorts them together.
-        return f'{self.directory}/{PREFIX}{self.bucket}.txt'
+        return Path(f'{self.directory}/{PREFIX}{self.bucket_name}.txt')
+
+    def _tail1(self, file):
+        return subprocess.check_output(['tail', '-1', file])
+
+    def _get_names(self, start_after):
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(self.bucket_name)
+        for obj in bucket.objects.all(start_after=start_after):
+            print(type(obj), dir(obj))
+            yield obj.key
+
+    def store_names(self):
+        start_after = ''
+        f = self._cache_file
+        if f.exists() and f.stat().st_size > 0:
+            start_after = self._tail1(f)
+        with open(f, 'a') as fout:
+            for name in self._get_names(start_after):
+                fout.write(f'{name}\n')
 
 
 @click.command()
