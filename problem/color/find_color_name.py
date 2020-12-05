e4d9b27926ed093be861efd6c1d6fcefef0a0594
John Hanley <john.hanley@parc.com>
2020-12-05 14:53:16 -0800

    Add distance_to_all_colors

--- problem/color/find_color_name.py
+++ problem/color/find_color_name.py
@@ -20,6 +20,8 @@
 
 from binascii import unhexlify
 from functools import lru_cache
+from operator import itemgetter
+import json
 
 import colormath.color_conversions as cv
 import colormath.color_diff as cd
@@ -30,7 +32,7 @@ assert 949 == len(sns.xkcd_rgb)  # e.g. 'very light green': '#d1ffbd'
 
 
 def _hex_to_rgb(hx: str) -> tuple:
-    hx = hx.replace('#', '')
+    hx = hx.lstrip('#')
     assert 6 == len(hx), hx
     r, g, b = unhexlify(hx)
     return r, g, b
@@ -52,15 +54,24 @@ def _red_check():
 
 
 @lru_cache()
-def get_color_by_name(name):
+def get_color_by_name(name) -> co.sRGBColor:
     return co.sRGBColor(*get_name_to_srgb()[name])
 
 
 def distance_between_colors(c1: co.sRGBColor,
-                            c2: co.sRGBColor):
+                            c2: co.sRGBColor) -> float:
     assert isinstance(c1, co.sRGBColor), 'pass a proper color, please'
     return cd.delta_e_cie2000(cv.convert_color(c1, co.LabColor),
                               cv.convert_color(c2, co.LabColor))
+
+
+def distance_to_all_colors(c1: co.sRGBColor, thresh_dist=22):
+    dist = {name: distance_between_colors(c1, get_color_by_name(name))
+            for name in sns.xkcd_rgb.keys()}
+    dist = {name: round(distance, 2)
+            for name, distance in sorted(dist.items(), key=itemgetter(1))
+            if distance < thresh_dist}
+    return dist
 
 
 if __name__ == '__main__':
@@ -70,3 +81,7 @@ if __name__ == '__main__':
     for name in 'bright red,red,pink,purple,violet,blue'.split(','):
         d = distance_between_colors(red, get_color_by_name(name))
         print(round(d, 1), name)
+
+    d = distance_to_all_colors(red)
+    print('')
+    print(json.dumps(d, indent=4))
