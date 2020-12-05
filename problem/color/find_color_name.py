6805c37b203f347ba1a2f88d3c838f6c8f96a4d8
John Hanley <john.hanley@parc.com>
2020-12-05 13:16:45 -0800

    Paint it red

--- problem/color/find_color_name.py
+++ problem/color/find_color_name.py
@@ -2,12 +2,24 @@
 
 from functools import lru_cache
 
-import seaborn
+import colormath as cm
+import colormath.color_objects as co
+import seaborn as sns
 
-assert 949 == len(seaborn.xkcd_rgb)  # e.g. 'very light green': '#d1ffbd'
+assert 949 == len(sns.xkcd_rgb)  # e.g. 'very light green': '#d1ffbd'
 
 
 @lru_cache()
-def get_name_to_rgb():
-    keys = seaborn.xkcd_rgb.keys()
-    return dict(zip(keys, seaborn.xkcd_palette(keys)))
+def get_name_to_srgb():
+    keys = sns.xkcd_rgb.keys()
+    return dict(zip(keys, sns.xkcd_palette(keys)))
+
+if __name__ == '__main__':
+    print(sns.xkcd_rgb['bright red'])
+    red = get_name_to_srgb()['bright red']
+    red = co.sRGBColor(*red)
+    print(red, red.is_upscaled)
+    print(co.sRGBColor(*red.get_value_tuple()),
+          co.sRGBColor(*red.get_value_tuple()).is_upscaled)
+    up = co.sRGBColor(*red.get_upscaled_value_tuple())
+    print(up, up.is_upscaled)
