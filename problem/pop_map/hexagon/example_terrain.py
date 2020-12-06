61f719e965f92b906725297abd3b87ab22bdaabb
John Hanley <john.hanley@parc.com>
2020-12-05 17:32:49 -0800

    Add hexagon/example_terrain

---
+++ problem/pop_map/hexagon/example_terrain.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+# Copyright 2020 John Hanley.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# The software is provided "AS IS", without warranty of any kind, express or
+# implied, including but not limited to the warranties of merchantability,
+# fitness for a particular purpose and noninfringement. In no event shall
+# the authors or copyright holders be liable for any claim, damages or
+# other liability, whether in an action of contract, tort or otherwise,
+# arising from, out of or in connection with the software or the use or
+# other dealings in the software.
+
+def _display_unicode_row(ch: str, n=4):
+    line = f'{ch}   ' * n
+    print(f"\n{line}\n  {line}\n{line}")
+
+
+def display_unicode_example():
+    hex_horiz = '\u2394'
+    hex_pointy = '\u2b21'
+    black_horiz = '\u2b23'
+    black_pointy = '\u2b22'
+    for hex in [hex_horiz, hex_pointy,
+                black_horiz, black_pointy]:
+        _display_unicode_row(hex)
+
+
+def display_tall_ascii_horiz_example(n=3):
+    ''
+
+
+if __name__ == '__main__':
+    display_unicode_example()
