c334b7caae2d130ea3e756075e8022a65dd02d4e
John Hanley <john.hanley@parc.com>
2021-01-09 13:27:02 -0800

    Use a more stringent max relative error

--- problem/numeric/sqrt_hypo_test.py
+++ problem/numeric/sqrt_hypo_test.py
@@ -25,23 +25,23 @@ from problem.numeric.sqrt import _relative_error
 from problem.numeric.sqrt import sqrt_binary_search as bin_sqrt
 from problem.numeric.sqrt import sqrt_newton_raphson as nr_sqrt
 
-ϵ = 1e-6
+ϵ = 1e-12
 big_float = 1.797e308
 
 
-@given(st.floats(min_value=ϵ ** 53, max_value=big_float))
+@given(st.floats(min_value=ϵ ** 26, max_value=big_float))
 def hypo_test_sqrt_rel_error_binary(n: float):
-    r = bin_sqrt(n)
-    assert abs(_relative_error(n, r * r)) < ϵ
+    r = bin_sqrt(n, rel_error=ϵ)
+    assert abs(_relative_error(r * r, n)) < ϵ
 
 
-@given(st.floats(min_value=ϵ ** 53, max_value=big_float))
+@given(st.floats(min_value=ϵ ** 26, max_value=big_float))
 def hypo_test_sqrt_rel_error_newton_raphson(n: float):
-    r = nr_sqrt(n)
-    assert abs(_relative_error(n, r * r)) < ϵ
+    r = nr_sqrt(n, rel_error=ϵ)
+    assert abs(_relative_error(r * r, n)) < ϵ
 
 
 if __name__ == '__main__':
-    assert ϵ ** 53 == 1e-318
+    assert ϵ ** 26 == 1e-312
     hypo_test_sqrt_rel_error_binary()
     hypo_test_sqrt_rel_error_newton_raphson()
