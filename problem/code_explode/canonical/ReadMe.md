42e497e8123f4eb122ad01f64d3c4b8882053237
John Hanley <john.hanley@parc.com>
2021-09-22 14:21:10 -0700

    Add isort; update to pre-commit hooks v3.4.0

---
+++ problem/code_explode/canonical/ReadMe.md
@@ -0,0 +1,3 @@
+
+Assesses how close python source is to "canonical" form,
+by round-tripping through bytecode and back to source code.
---
+++ problem/code_explode/canonical/report.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import re
+
+import os
+
+from pathlib import Path
+
+def canonical_report(infile='report.py', outdir='out'):
+    ''
+
+
+
+if __name__ == '__main__':
+    canonical_report()
