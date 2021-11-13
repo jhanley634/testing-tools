a5bae433f65f4d9632ad68a130d7591d8f961f72
John Hanley <jhanley@rexhomes.com>
2021-11-12 18:00:57 -1000

    1st attempt to call load_boston()

--- problem/numeric/us_home_sales/boston.py
+++ problem/numeric/us_home_sales/boston.py
@@ -18,34 +18,15 @@
 # arising from, out of or in connection with the software or the use or
 # other dealings in the software.
 #
-from sklearn.model_selection import KFold, cross_val_score
-from xgboost import XGBRegressor
-import matplotlib
-import matplotlib.pyplot as plt
-import sklearn
-import xgboost as xgb
+from sklearn.datasets import load_boston
 
-from problem.numeric.us_home_sales.us_home_sales import _get_sales_subset
+# https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_boston.html
 
 
-def find_informative_attrs():
-    df = _get_sales_subset()
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-        df.copy().drop(['saleprice'], axis=1), df.saleprice)
-    model = XGBRegressor(max_depth=20, n_estimators=500)
-    kfold = KFold(n_splits=5)
-    results = cross_val_score(model, x_train, y_train, cv=kfold)
-    print("Accuracy: %.2f%% (%.2f%%)" % (results.mean() * 100, results.std() * 100))
-
-    model.fit(x_train, y_train)
-    matplotlib.use('MacOSX')
-    fig, ax = plt.subplots(2)
-    xgb.plot_importance(model, ax=ax[0], max_num_features=7)
-
-    y_pred = model.predict(x_test)
-    plt.scatter(y_test, y_pred)
-    plt.show()
+# from https://www.datacamp.com/community/tutorials/xgboost-in-python
+def predict_boston_home_prices():
+    print(load_boston())
 
 
 if __name__ == '__main__':
-    find_informative_attrs()
+    predict_boston_home_prices()
