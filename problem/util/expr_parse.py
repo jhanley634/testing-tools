372c24ddac8ac7b4ffe2ffc3f788e6206b07376a
John Hanley <jhanley708-git@s6.sector6.net>
2022-05-17 07:51:57 -0700

    Use a deque

--- problem/util/expr_parse.py
+++ problem/util/expr_parse.py
@@ -1,4 +1,5 @@
 
+from collections import deque
 import re
 
 WrapperDescriptor = type(object.__init__)
@@ -52,6 +53,29 @@ class ExprParser:
             else:
                 return f'{token}'
 
-        tokens = [to_str(token)
-                  for token, _ in self._get_tokens()]
-        return ' '.join(tokens)
+        expr = []  # a postfix expression
+        stack = deque()  # stuff we've not gotten around to yet
+        for token, prec in self._get_tokens():
+            prec = self.op_to_precedence.get(token)
+            if isinstance(token, int):
+                expr.append(token)
+            elif token == '(':
+                stack.append(token)
+            elif token == ')':
+                assert stack[-1] != ')', stack  # Prohibit '()', and also ')'.
+                while stack[-1] != ')':
+                    tok = stack.pop()
+                    expr.append(tok)
+            elif prec:
+                if len(stack) == 0 or stack[-1] == '(':
+                    stack.append(token)
+                else:
+                    while self.op_to_precedence.get(stack[-1], 99) < prec:
+                        expr.append(stack.pop())
+            else:
+                stack.append(token)
+
+        while len(stack) > 0:
+            expr.append(stack.pop())
+
+        return ' '.join(map(to_str, expr))
