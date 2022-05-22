2847ea61c77766b11b4c90f31131aceffab54510
John Hanley <jhanley708-git@s6.sector6.net>
2022-05-21 19:24:17 -0700

    Add an "empty stack" guard

--- problem/util/expr_parse.py
+++ problem/util/expr_parse.py
@@ -56,26 +56,30 @@ class ExprParser:
         expr = []  # a postfix expression
         stack = deque()  # stuff we've not gotten around to yet
         for token, prec in self._get_tokens():
-            prec = self.op_to_precedence.get(token)
+            prec = self.op_to_precedence.get(to_str(token))
+            # print(prec, token)
             if isinstance(token, int):
                 expr.append(token)
             elif token == '(':
                 stack.append(token)
             elif token == ')':
                 assert stack[-1] != ')', stack  # Prohibit '()', and also ')'.
-                while stack[-1] != ')':
+                while stack and stack[-1] != ')':
                     tok = stack.pop()
-                    expr.append(tok)
+                    if tok != '(':
+                        expr.append(tok)
             elif prec:
                 if len(stack) == 0 or stack[-1] == '(':
                     stack.append(token)
                 else:
-                    while self.op_to_precedence.get(stack[-1], 99) < prec:
+                    print(8, expr)
+                    print(9, stack)
+                    while stack and self.op_to_precedence.get(to_str(stack[-1]), 99) < prec:
                         expr.append(stack.pop())
             else:
                 stack.append(token)
 
-        while len(stack) > 0:
+        while stack:
             expr.append(stack.pop())
 
         return ' '.join(map(to_str, expr))
