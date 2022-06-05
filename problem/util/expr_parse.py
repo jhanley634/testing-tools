380b674a0ed000b8dd85ef724a64c0e54abf329b
John Hanley <jhanley708-git@s6.sector6.net>
2022-06-04 18:45:10 -0700

    Add _div

--- problem/util/expr_parse.py
+++ problem/util/expr_parse.py
@@ -5,11 +5,17 @@ import re
 WrapperDescriptor = type(object.__init__)
 
 
+def _div(a, b):
+    return a / b
+
+
 def _get_op_pairs():
     yield from {
         '(': (0, None),
         '+': (1, float.__add__),
+        '-': (1, float.__sub__),
         '*': (2, float.__mul__),
+        '/': (2, _div),
         '^': (3, float.__pow__),
     }.items()
 
@@ -21,7 +27,7 @@ class ExprParser:
     op_to_fn = {
         op: pair[1] for op, pair in _get_op_pairs()}
 
-    sane_charset_re = re.compile(r'^[()+*^0-9 ]+$')
+    sane_charset_re = re.compile(r'^[()+*/^0-9 -]+$')
 
     def __init__(self, infix: str):
         assert self.sane_charset_re.search(infix), infix
@@ -70,7 +76,7 @@ class ExprParser:
         return ' '.join(map(self._to_str, expr))
 
     def _to_str(self, token):
-        if isinstance(token, WrapperDescriptor):
+        if isinstance(token, WrapperDescriptor) or token is _div:
             txt = ' UNKNOWN '
             for k, v in self.op_to_fn.items():
                 if v == token:
@@ -89,8 +95,9 @@ class ExprParser:
                 stack.append(float(int(tok)))
             else:
                 op = self.op_to_fn[self._to_str(tok)]
-                stack.append(op(stack.pop(),
-                                stack.pop()))
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(op(a, b))
 
         assert 1 == len(stack), (len(stack), stack)
         return stack.pop()
