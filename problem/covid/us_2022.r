6be540d9a6fc2a65f1418420bf9470936d4c48a5
John Hanley <jhanley708-git@s6.sector6.net>
2022-07-02 17:26:21 -0700

    Ensure the date column is of type Date

--- problem/covid/us_2022.r
+++ problem/covid/us_2022.r
@@ -1,5 +1,6 @@
 #! /usr/bin/env Rscript
 
+library(dplyr, warn.conflicts = FALSE)
 library(ggplot2)
 library(ggrepel)
 
@@ -12,11 +13,11 @@ if (FALSE) {
 }
 
 fspec <- "../covid-19-data/us-states.csv"
-states <- read.csv(fspec)
+states <- read.csv(fspec) %>%
+  mutate(date = as.Date(date))
 
 g <- ggplot(states, aes(x = date, y = cases)) +
   geom_point() +
-  scale_x_date(breaks = seq(as.Date("2019-01-01"),
-                            as.Date("2023-01-01"),
-                            by = "91 days"))
+  scale_x_date(breaks ="2 months") +
+  theme(axis.text.x = element_text(angle = 90))
 g
