c88095ca7da489117cafa6941eeb2e19d7601254
John Hanley <jhanley708-git@s6.sector6.net>
2022-08-05 14:32:08 -0700

    On some problems, choosing regex package over re yields big speedup

--- problem/covid/lag/pos_tagging.py
+++ problem/covid/lag/pos_tagging.py
@@ -5,19 +5,19 @@ from urllib.parse import urlparse
 import os.path
 import re
 
-from bs4 import BeautifulSoup
 from html2text import html2text
+import regex
 import requests
 import spacy
 import typer
 
 
-def article_urls():
+def _article_urls():
     return [
         ('https://www.nytimes.com/interactive/2022/07/07/us/ba5-covid-omicron-subvariant.html',
          'What the BA.5 Subvariant Could Mean for the United States'),
         ('https://www.nytimes.com/interactive/2021/us/covid-cases.html',
-         ''),
+         'Coronavirus in the U.S.: Latest Map and Case Count'),
     ]
 
 
@@ -42,19 +42,18 @@ def _get_fspec(url, folder='/tmp'):
 
 
 def fetch_articles():
-    skip_to_content_re = re.compile(r'.*<main id="site-content">')
+    skip_to_content_re = regex.compile(r'.*?<main id="site-content">')
 
     nlp = spacy.load('en_core_web_sm')  # $ python -m spacy download en_core_web_sm
 
-    for url, title in article_urls():
-        html = skip_to_content_re.sub('<p>hi there!</p>', _get_article_text(url))
-        soup = BeautifulSoup(html, 'html.parser')
-        print('\n\n====\n', url, title, html2text(soup.prettify()))
+    for url, title in _article_urls():
+        html = _get_article_text(url)
+        html = skip_to_content_re.sub('', html)
+        print('\n\n====\n', url, title)
 
-        doc = nlp(html2text(soup.prettify()))
+        doc = nlp(html2text(html))
         for ent in doc.ents:
-            print(ent)
-            print(ent.label_, ent.text)
+            print(f'\n\n{ent.label_:<12} {ent.text}')
 
 
 if __name__ == '__main__':
