7fea30fd0b81c5ba1a8e329630a93ead4ca7fca9
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-15 06:51:07 -0800

    Add _run_freeze

--- problem/conda_env_yml/pip_freeze_requirements.py
+++ problem/conda_env_yml/pip_freeze_requirements.py
@@ -1,30 +1,43 @@
 #! /usr/bin/env python
 # Copyright 2023 John Hanley. MIT licensed.
 
+
+from pathlib import Path
 from pprint import pp
-import glob
-import os
+from typing import Generator, Iterable
 import re
 import subprocess
 
 from ruamel.yaml import YAML
 
 
-def _run_freeze():
-    dep_ver_re = re.compile(r"^([\w-]+)==([\d.]+)$")
+def _parse_requirements(in_file: Path) -> Generator[tuple[str, str], None, None]:
+    with open(in_file) as fin:
+        yield from _dep_ver_tuples(fin)
+
+
+def _run_freeze() -> Generator[tuple[str, str], None, None]:
     cmd = "pip freeze"
-    for line in subprocess.check_output(cmd.split()).decode().split("\n"):
-        m = dep_ver_re.search(line.rstrip())
+    frozen = subprocess.check_output(cmd.split()).decode().split("\n")
+    yield from _dep_ver_tuples(frozen)
+
+
+_dep_ver_re = re.compile(r"^([\w-]+)[~>=]=([\d.]+)$")
+
+
+def _dep_ver_tuples(fin: Iterable[str]) -> Generator[tuple[str, str], None, None]:
+    for line in fin:
+        m = _dep_ver_re.search(line)
         if m:
-            yield m.groups()  # e.g. 'absl-py': '2.0.0'
+            dep, ver = m.groups()  # e.g. 'absl-py': '2.0.0'
+            yield dep, ver
 
 
-def _get_frozen_dep_ver():
-    return dict(_run_freeze())
-
-
-def main():
-    pp(_get_frozen_dep_ver())
+def main() -> None:
+    req = dict(_parse_requirements(Path("requirements.txt")))
+    assert 28 == len(req)
+    freeze = dict(_run_freeze())
+    pp(freeze)
 
 
 if __name__ == "__main__":
