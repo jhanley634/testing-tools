884c6f4413e404466745fa3b052009d885f8a909
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-15 07:14:16 -0800

    Add _parse_environment

--- problem/conda_env_yml/pip_freeze_requirements.py
+++ problem/conda_env_yml/pip_freeze_requirements.py
@@ -3,41 +3,56 @@
 
 
 from pathlib import Path
-from pprint import pp
 from typing import Generator, Iterable
 import re
 import subprocess
 
-from ruamel.yaml import YAML
+_dep_ver_re = re.compile(r"^([\w-]+)[~>=]=([\d.]+)$")
 
 
 def _parse_requirements(in_file: Path) -> Generator[tuple[str, str], None, None]:
     with open(in_file) as fin:
-        yield from _dep_ver_tuples(fin)
+        yield from _dep_ver_tuples(fin, _dep_ver_re)
+
+
+def _parse_environment(in_file: Path) -> Generator[tuple[str, str], None, None]:
+    dep_ver_re = re.compile(r"^ *- *([\w-]+) *>?= *([\d.]+)")
+    with open(in_file) as fin:
+        yield from _dep_ver_tuples(fin, dep_ver_re)
 
 
 def _run_freeze() -> Generator[tuple[str, str], None, None]:
     cmd = "pip freeze"
     frozen = subprocess.check_output(cmd.split()).decode().split("\n")
-    yield from _dep_ver_tuples(frozen)
+    yield from _dep_ver_tuples(frozen, _dep_ver_re)
 
 
-_dep_ver_re = re.compile(r"^([\w-]+)[~>=]=([\d.]+)$")
-
-
-def _dep_ver_tuples(fin: Iterable[str]) -> Generator[tuple[str, str], None, None]:
+def _dep_ver_tuples(
+    fin: Iterable[str], dep_ver_re: re.Pattern
+) -> Generator[tuple[str, str], None, None]:
     for line in fin:
-        m = _dep_ver_re.search(line)
+        m = dep_ver_re.search(line)
         if m:
             dep, ver = m.groups()  # e.g. 'absl-py': '2.0.0'
-            yield dep, ver
+            yield dep.lower(), ver
 
 
 def main() -> None:
     req = dict(_parse_requirements(Path("requirements.txt")))
     assert 28 == len(req)
     freeze = dict(_run_freeze())
-    pp(freeze)
+
+    for dep, rev in _parse_environment(Path("environment.yml")):
+        fr_rev = freeze.get(dep, "0.0.0")
+        if fr_rev > rev:
+            print(f"UPDATE environment.yml to use: {dep} >= {fr_rev}")
+
+        if dep not in req:
+            print(f"{dep}=={rev}")
+        else:
+            if req[dep] < rev:
+                dep = dep.upper()
+                print(f"{dep}=={rev}")
 
 
 if __name__ == "__main__":
